22c8fb2542b9a4999e1125543d8e63a124e0efc4
ARBONA Julien <julien.arbona@outlook.fr>
2022-03-04 16:39:22 +0100

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,67 @@
+let el_form = document.querySelector('form');
+let el_line = document.querySelector('#email_line')
+let el_email = document.querySelector('input[name=email]');
+let el_btn = document.querySelector('#email_line .btn');
+let el_errorIcon = document.querySelector('#email_line .error-icon');
+let el_errorMsg = document.querySelector('#email_line .error-msg');
+
+el_email.addEventListener('focusin', (event) => {
+    el_line.classList.add('form-line--high');
+    el_btn.classList.add('btn--high');
+    el_errorMsg.classList.add('error-msg--high');
+});
+
+el_email.addEventListener('focusout', (event) => {
+    if (el_email.value) {
+        if (validationEmail(el_email.value)) {
+            el_line.classList.remove('form-line--high');
+            el_btn.classList.remove('btn--high');
+            el_errorMsg.classList.remove('error-msg--high');
+            
+        }
+    }else {
+        el_line.classList.remove('form-line--high');
+        el_btn.classList.remove('btn--high');
+        el_errorMsg.classList.remove('error-msg--high');
+
+    }
+});
+
+el_form.addEventListener('submit', (event) => {
+    if (el_email.value) {
+        if (validationEmail(el_email.value)) {
+            hideError();
+            el_email.value = "";
+            console.log('Submit ! ');
+        }else {
+            showError();
+        }
+        
+    }
+    // Stop submit form
+    event.preventDefault();
+});
+
+function validationEmail(email) {
+    let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+    
+    if (regex.test(email)) {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+function showError() {
+    el_errorIcon.style.display = 'block';
+    el_errorMsg.style.display = 'block';
+    el_line.classList.add('form-line--error');
+    
+}
+
+function hideError() {
+    el_errorIcon.style.display = 'none';
+    el_errorMsg.style.display = 'none';
+    el_line.classList.remove('form-line--error');
+
+}
